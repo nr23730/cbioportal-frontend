d03cfec228227670e565aa16e4aeecb491020343
lmahl <mahlmeister@DESKTOP-S3GK7DV.localdomain>
2020-07-22 15:51:22 +0200

    Add Interface for Coordinate distance calculation

---
+++ src/pages/patientView/clinicalTrialMatch/utils/location/CoordinateList.ts
@@ -0,0 +1,7 @@
+import { Coordinate } from './coordinate';
+
+const cities_and_coordinates: { [name: string]: Coordinate } = {
+    Erlangen: new Coordinate(49.6, 11.0),
+    Leipzig: new Coordinate(51.359, 12.377),
+    Dresden: new Coordinate(51.05, 13.739),
+};
